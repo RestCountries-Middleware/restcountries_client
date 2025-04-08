d81e66d9dc854c4de3524aa17e11110965484389
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-04-09 04:48:12 +0530

    bootstrap icons added

--- src/App.tsx
+++ src/App.tsx
@@ -6,39 +6,49 @@ import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import ApiKeysPage from "./pages/ApiKeys";
 import ApiExamplePage from "./pages/ApiExample";
+import { ToastContainer } from "react-toastify";
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/apikeys"
+          element={
+            <ProtectedRoute>
+              <ApiKeysPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/apiexample"
+          element={
+            <ProtectedRoute>
+              <ApiExamplePage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        theme="light"
+        closeOnClick
+        pauseOnHover
       />
-      <Route
-        path="/apikeys"
-        element={
-          <ProtectedRoute>
-            <ApiKeysPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/apiexample"
-        element={
-          <ProtectedRoute>
-            <ApiExamplePage />
-          </ProtectedRoute>
-        }
-      />
-      Ç
-    </Routes>
+    </>
   );
 };
 
