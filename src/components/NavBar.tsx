5d766102e208900cf8679eb35277ae37f4e89e54
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-04-08 21:28:04 +0530

    navbar added

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,72 @@
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+const Navbar: React.FC = () => {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    sessionStorage.clear();
+    localStorage.clear();
+    navigate("/login");
+  };
+
+  const userEmail = localStorage.getItem("userEmail");
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark px-5 py-3">
+        <Link className="navbar-brand text-white me-5" to="/dashboard">
+          <img src="src/assets/logo.png" height={30} alt="" />
+        </Link>
+
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/dashboard"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                Dashboard
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/apikeys"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                API Keys
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/apiexample"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                API Example
+              </NavLink>
+            </li>
+          </ul>
+          <div className="d-flex align-items-center gap-3">
+            {userEmail && (
+              <span className="text-white small me-3">
+                Logged in as: <strong>{userEmail}</strong>
+              </span>
+            )}
+            <button className="btn px-3 py-1 btn-outline-danger" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
