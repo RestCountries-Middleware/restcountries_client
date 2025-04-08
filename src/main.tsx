c416853387177d2d423c3d9b96136139be5103fe
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-04-09 04:48:48 +0530

    bootstrap icons added

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { AuthProvider } from './contexts/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
